c76960bbd2517f848d732b661bc629ff113840b1
Michael Chrsitie <mikeychristie@gmail.com>
2020-12-29 21:35:38 +0000

    Commit message

--- App.js
+++ App.js
@@ -1,25 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState}  from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList} from 'react-native';
+
+import GoalItem from './Components/GoalItem';
 
 export default function App() {
 
-  const [outputText, setOutputText] = useState('this is a test');
+  const [enteredGoal, setEnteredGoal] = useState('');
+
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () =>{
+    console.log(enteredGoal)
+    setCourseGoals(currentGoals => [
+      ...currentGoals, 
+      { id: Math.random().toString(), value: enteredGoal}
+    ]);
+  };
 
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text!" onPress={() => setOutputText('The Text Ccfghhanged')}/>
-      <StatusBar style="auto" />
-    </View>
+    <View style={styles.screen}>
+         <View style={styles.inputContainer}>
+          <TextInput
+          placeholder="Enter Goal Here" 
+          style = {styles.input} 
+          onChangeText={goalInputHandler}
+          value = {enteredGoal}
+          />
+          <Button title = "Add" onPress={addGoalHandler} />
+        </View>
+
+        <FlatList 
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+         renderItem={itemData => GoalItem title = />}
+
+         />
+
+         </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  screen: {
+    padding: 80
   },
+
+  inputContainer:{
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems:'center'
+  },
+
+  input:{
+    width: '80%', 
+    borderColor: 'black',
+    borderWidth: 1,  
+    padding: 10
+  },
+
+  listItem: {
+    padding: 10,
+    margin: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
+
+  
+
+  
 });
+
+
